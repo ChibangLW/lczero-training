465a202156513a50d5acfbf91076265db4a79b97
Léon Lenzen <lenzen@zafaco.de>
2021-02-18 23:18:02 +0100

    fix: generating agent config with barebone base config

--- tf/util.py
+++ tf/util.py
@@ -9,6 +9,7 @@ import random
 def get_config_file_name(agent_uuid: int, config_dir):
     return config_dir+"agent_"+str(agent_uuid)+".yaml"
 
+
 def delete_agent_config_files(config_dir: str):
     fileList = glob.glob(config_dir + "agent_*.yaml")
     # Iterate over the list of filepaths & remove each file.
@@ -39,19 +40,22 @@ def create_config_file(agent_id: int, config_dir, pbt_input_base_dir):
             cfg['training']['checkpoint_steps'] = evolution_size
             cfg['training']['test_steps'] = int(1000)
 
+            cfg['model'] = {
+                'filters': int(network["filters"]),
+                'policy_channels': int(network["policy_channels"]),
+                'residual_blocks': int(network["residual_blocks"]),
+                'se_ratio': int(network["se_ratio"])
+            }
 
-            cfg['model']['filters'] = int(network["filters"])
-            cfg['model']['policy_channels'] = int(network["policy_channels"])
-            cfg['model']['residual_blocks'] = int(network["residual_blocks"])
-            cfg['model']['se_ratio'] = int(network["se_ratio"])
+            cfg['dataset'] = {
+                'input_train': get_pbt_train_path(
+                    evolution["iteration"], pbt_input_base_dir),
+                'input_test': get_pbt_test_path(
+                    evolution["iteration"], pbt_input_base_dir),
+                'num_chunks': evolution_size * 25,
+                'train_ratio': float(20/25)
+            }
 
-            cfg['dataset']['input_train'] = get_pbt_train_path(evolution["iteration"], pbt_input_base_dir)
-            cfg['dataset']['input_test'] = get_pbt_test_path(evolution["iteration"], pbt_input_base_dir)
-            cfg['dataset']['num_chunks'] = evolution_size * 25
-            cfg['dataset']['train_ratio'] = float(20/25)
-
-            # cfg['training']['policy_loss_weight'] = policy_loss_weight
-            # cfg['training']['value_loss_weight'] = value_loss_weight
             yaml.dump(cfg, config_file)
 
 
@@ -59,70 +63,82 @@ def get_base_network_path(agent_id: int, network_dir, name, steps: int) -> str:
     agent = models.get_agent(agent_id)
     return network_dir + name + "_" + str(agent["uuid"]) + "/" + name + "_" + str(agent["uuid"]) + "-" + str(steps)
 
-def get_weights_file(agent_id: int, network_dir, name, steps: int) -> str: 
-    return get_base_network_path(agent_id, network_dir, name,steps) + ".pb.gz"
+
+def get_weights_file(agent_id: int, network_dir, name, steps: int) -> str:
+    return get_base_network_path(agent_id, network_dir, name, steps) + ".pb.gz"
+
 
 def get_weights_index_file(agent_id: int, network_dir, name, steps: int) -> str:
-    return get_base_network_path(agent_id, network_dir, name,steps) + ".index"
+    return get_base_network_path(agent_id, network_dir, name, steps) + ".index"
+
 
 def get_weights_data_file(agent_id: int, network_dir, name, steps: int) -> str:
-    return get_base_network_path(agent_id, network_dir, name,steps) + ".data-00000-of-00001"
+    return get_base_network_path(agent_id, network_dir, name, steps) + ".data-00000-of-00001"
+
 
 def get_pbt_train_path(iteration: int, base_dir: str):
     return base_dir + get_pbt_train_folder(iteration)
 
+
 def get_pbt_train_folder(iteration: int):
     return "evo_" + str(iteration) + "/train/"
+
 
 def get_pbt_test_path(iteration: int, base_dir: str):
     return base_dir + get_pbt_test_folder(iteration)
 
+
 def get_pbt_test_folder(iteration: int):
     return "evo_" + str(iteration) + "/test/"
 
+
 def prepare_training_data(evolution_num: int, evolution_size: int, train_data_dir: str, test_data_dir: str, output_dir: str):
     print("Preparing data for " + str(evolution_num) + " evolutions...")
-    
+
     try:
         shutil.rmtree(output_dir)
     except FileNotFoundError:
         print("No previous data found")
 
-    for evolution in range(evolution_num): 
+    for evolution in range(evolution_num):
         # current_evolution = int(evolution) + int(1)
         train_path = os.path.join(output_dir, get_pbt_train_folder(evolution))
         # try:
-            # os.makedirs(train_path)
+        # os.makedirs(train_path)
         # except FileExistsError:
-            # shutil.rmtree(train_path)
-            # os.makedirs(train_path)
+        # shutil.rmtree(train_path)
+        # os.makedirs(train_path)
         os.makedirs(train_path)
 
         test_path = os.path.join(output_dir, get_pbt_test_folder(evolution))
         # try:
-            # os.makedirs(test_path)
+        # os.makedirs(test_path)
         # except FileExistsError:
-            # shutil.rmtree(test_path)
-            # os.makedirs(test_path)
+        # shutil.rmtree(test_path)
+        # os.makedirs(test_path)
         os.makedirs(test_path)
 
-        ## Train data
+        # Train data
         train_file_list = _get_file_list(train_data_dir)
-        train_random_files = _get_random_files(train_file_list, evolution_size*20)
+        train_random_files = _get_random_files(
+            train_file_list, evolution_size*20)
         _copy_files(train_random_files, train_data_dir, train_path)
 
-        ## Train data
+        # Train data
         test_file_list = _get_file_list(test_data_dir)
         test_random_files = _get_random_files(test_file_list, evolution_size*5)
         _copy_files(test_random_files, test_data_dir, test_path)
 
         print("Evolution " + str(evolution) + " done")
 
+
 def _get_file_list(input_dir):
     return [file for file in os.listdir(input_dir) if os.path.isfile(os.path.join(input_dir, file))]
 
+
 def _get_random_files(file_list, N):
     return random.sample(file_list, N)
+
 
 def _copy_files(random_files, input_dir, output_dir):
     for file in random_files:
